afbc300ea97574e8645520adc1e804e87edfa238
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2013-07-30 23:36:07 -0700

    Initial Rails App

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Crowdvocate::Application.config.secret_key_base = '32acea80ec4d3f1e5c0b987b30ada496a9c9179f3e3606fe3bfea7a4a94d80109b3ae009debbc6ee32748271eea7a8b2048dc1326d41d645fc7b42d8fb5aecdb'
