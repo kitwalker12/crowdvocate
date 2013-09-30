997dcbf5b493ad0b342c835dda09bfcac0743a14
Kunwar Aditya Raghuwanshi <adi.version1@gmail.com>
2013-09-29 19:27:54 -0700

    active admin to_param fix

--- app/admin/project.rb
+++ app/admin/project.rb
@@ -1,4 +1,9 @@
 ActiveAdmin.register Project do
+
+  before_filter :only => [:show, :edit, :update] do
+    @project = Project.find_by_slug(params[:id])
+  end
+
   controller do
     def permitted_params
       params.permit!
